997f6497d6376dc59f9b289f5766e45e0f61312f
NataliiaShah <gavrilovskaboroluk@gmail.com>
2024-10-03 17:35:55 +0300

    Update task-6.js
    correction

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,14 @@
+const numberValue = document.querySelector('input[type="number"]');
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
